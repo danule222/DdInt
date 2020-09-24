6310f7b2c3de7c83af2868be4f4211d3fa8f1cce
Daniel Ramírez Morilla <daniel@xarblanca.es>
2020-09-24 13:46:51 +0200

    Update

--- pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
+++ pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
@@ -4,11 +4,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ventanaLogin {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -38,11 +46,47 @@ public class ventanaLogin {
 	 */
 	private void initialize() {
 		frame = new JFrame("Inicio de sesión");
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 282, 194);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		JLabel lblNewLabel = new JLabel("Usuario");
+		frame.getContentPane().add(lblNewLabel, "4, 4");
+		
+		textField = new JTextField();
+		frame.getContentPane().add(textField, "6, 4, 3, 1, left, default");
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a");
+		frame.getContentPane().add(lblNewLabel_1, "4, 6");
+		
+		textField_1 = new JTextField();
+		frame.getContentPane().add(textField_1, "6, 6, 3, 1, left, default");
+		textField_1.setColumns(10);
+		
+		btnNewButton = new JButton("Inicar sesi\u00F3n");
+		frame.getContentPane().add(btnNewButton, "4, 10");
 	}
 
 }
