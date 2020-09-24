9c7794e4c0ee7fc4bb4c178df8f4cc5d85514419
Daniel Ramírez Morilla <daniel@xarblanca.es>
2020-09-24 17:39:52 +0200

    Tarea finalizada.

--- pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
+++ pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
@@ -9,30 +9,30 @@ import javax.swing.JButton;
 import java.sql.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class ventanaLogin {
 
+	static Connection conexion;
+	
 	private JFrame frame;
 	private JLabel lblUsuario;
-	private JTextField textField;
+	private JTextField tfUsuario;
 	private JLabel lblContrasenna;
-	private JTextField textField_1;
+	private JTextField tfContrasenna;
 	private JButton btnLogin;
 
 	/**
-	 * Launch the application.
+	 * Iniciar la aplicación.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					conectarBDD();
 					ventanaLogin window = new ventanaLogin();
 					window.frame.setVisible(true);
-					try{  
-						Class.forName("com.mysql.jdbc.Driver");  
-						Connection conexion = DriverManager.getConnection(  
-						"jdbc:mysql://localhost:3306/pruebainicialdi","daniel","7880");
-						}catch(Exception e){ System.out.println(e);}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -41,14 +41,14 @@ public class ventanaLogin {
 	}
 
 	/**
-	 * Create the application.
+	 * Crear la aplicación.
 	 */
 	public ventanaLogin() {
 		initialize();
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Inicializar el contenido de la ventana.
 	 */
 	private void initialize() {
 		frame = new JFrame("Inicio de sesión");
@@ -60,30 +60,85 @@ public class ventanaLogin {
 		lblUsuario.setBounds(10, 13, 95, 14);
 		frame.getContentPane().add(lblUsuario);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(91, 11, 165, 20);
-		frame.getContentPane().add(textField);
+		tfUsuario = new JTextField();
+		tfUsuario.setColumns(10);
+		tfUsuario.setBounds(91, 11, 165, 20);
+		frame.getContentPane().add(tfUsuario);
 		
 		lblContrasenna = new JLabel("Contrase\u00F1a");
 		lblContrasenna.setBounds(10, 40, 95, 14);
 		frame.getContentPane().add(lblContrasenna);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(91, 38, 165, 20);
-		frame.getContentPane().add(textField_1);
+		tfContrasenna = new JTextField();
+		tfContrasenna.setColumns(10);
+		tfContrasenna.setBounds(91, 38, 165, 20);
+		frame.getContentPane().add(tfContrasenna);
 		
-		// ACCIÓN AL PRESIONAR EL BOTÓN "INICIAR SESIÓN"
+		JLabel msgSistema = new JLabel("");
+		msgSistema.setHorizontalAlignment(SwingConstants.CENTER);
+		msgSistema.setForeground(Color.GRAY);
+		msgSistema.setBounds(10, 65, 246, 14);
+		frame.getContentPane().add(msgSistema);
 		
-		btnLogin = new JButton("Inicar sesi\u00F3n");
+		btnLogin = new JButton("Iniciar sesi\u00F3n");
 		btnLogin.addActionListener(new ActionListener() {
+			
+			/**
+			 * Genera una consulta en la que se comprueba con los datos introducidos
+			 * en los campos "Usuario" y "Contraseña" si el usuario existe en la base
+			 * de datos.
+			 */
 			public void actionPerformed(ActionEvent e) {
+				String usuario = tfUsuario.getText();
+				String contrasenna = tfContrasenna.getText();
 				
+				Statement consulta;
+				ResultSet resultado = null;
+				try {
+					consulta = conexion.createStatement();
+					resultado = consulta.executeQuery("SELECT count(nombre) FROM usuarios WHERE nombre = \"" + usuario
+							+ "\" AND contrasenna = \"" + contrasenna + "\";");
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+					System.out.println("Problema al crear consulta.");
+				}
+				String vof = null;
+				try {
+					while (resultado.next()) {
+					    
+						try {
+							vof = resultado.getString("count(nombre)");
+						} catch (SQLException e1) {
+							e1.printStackTrace();
+						}
+					}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				System.out.println(usuario + "\n" + contrasenna);
+				System.out.println(vof);
+				if (vof.equals("1")) {
+					msgSistema.setText("Usuario existente.");
+				} else {
+					msgSistema.setText("Usuario no existente.");
+				}
 			}
 		});
 		btnLogin.setBounds(71, 85, 125, 23);
 		frame.getContentPane().add(btnLogin);
 	}
-
+	
+	/**
+	 * Establecer contacto con la base de datos.
+	 */
+	public static void conectarBDD() {
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			conexion = DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/pruebainicialdi?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+			"daniel","7880");
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
