e485d4a74f6b9605098d6cfac8d4da3a702c9703
Daniel Ramírez Morilla <daniel@xarblanca.es>
2020-09-24 14:33:27 +0200

    Conexión a BDD

--- pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
+++ pruebaInicialDI/src/pruebaInicialDI/ventanaLogin.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.sql.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ventanaLogin {
 
@@ -25,6 +28,11 @@ public class ventanaLogin {
 				try {
 					ventanaLogin window = new ventanaLogin();
 					window.frame.setVisible(true);
+					try{  
+						Class.forName("com.mysql.jdbc.Driver");  
+						Connection conexion = DriverManager.getConnection(  
+						"jdbc:mysql://localhost:3306/pruebainicialdi","daniel","7880");
+						}catch(Exception e){ System.out.println(e);}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -66,7 +74,14 @@ public class ventanaLogin {
 		textField_1.setBounds(91, 38, 165, 20);
 		frame.getContentPane().add(textField_1);
 		
+		// ACCIÓN AL PRESIONAR EL BOTÓN "INICIAR SESIÓN"
+		
 		btnLogin = new JButton("Inicar sesi\u00F3n");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnLogin.setBounds(71, 85, 125, 23);
 		frame.getContentPane().add(btnLogin);
 	}
